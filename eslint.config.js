190962bdf5eae2621739f5b0176d3d91bd390d9f
Fery Wardiyanto <ferywardiyanto@gmail.com>
2024-09-04 09:47:25 +0700

    chore: init development configs
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+'use strict'
+
+import antfu from '@antfu/eslint-config'
+
+export default antfu(
+  {
+    ignores: [
+      'tests/reports/**',
+    ],
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+)
